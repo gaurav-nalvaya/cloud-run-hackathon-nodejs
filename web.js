fd5749b452c1820e24655c788c8fe5220e5c49ea
Gaurav Nalvaya <46955692+gaurav-nalvaya@users.noreply.github.com>
2022-11-18 14:57:01 +0530

    Update web.js

--- web.js
+++ web.js
@@ -9,9 +9,9 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log('log of request start')
+  console.log('log of request start');
   console.log(req.body);
-  console.log('log of request end')
+  console.log('log of request end');
   const moves = ['F', 'T', 'L', 'R'];
   
   // TODO add your implementation here to replace the random response
